b4e30da076e6591b7dbbb8168c6570c45d3e063d
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-02-06 11:54:22 +0900

    게시글 쓰기 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -3,14 +3,18 @@ package shop.mtcoding.blog.board;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
+import java.awt.*;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
@@ -18,6 +22,34 @@ public class BoardController {
 
     private final BoardRepository boardRepository;
     private final HttpSession session;
+
+
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
+
+        // 1. 인증 체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        // 2. 바디 데이터 확인 및 유효성 검사
+        log.info(requestDTO.toString());
+
+        if (requestDTO.getTitle().length() > 30) {
+            request.setAttribute("status", 400);
+            request.setAttribute("msg", "title의 길이가 30자를 초과해선 안 돼요");
+            return "error/40x";
+        }
+
+        // 3. 모델 위임
+        // insert into board_tb(title, content, user_id, created_at)values(?, ?, ?, now());
+
+        boardRepository.save(requestDTO, sessionUser.getId());
+
+        return "redirect:/";
+    }
 
     @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
